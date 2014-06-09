da7cc5b1dcbc7b80b6e18738f46f2ed2749d4bd9
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2014-06-09 16:36:00 -0700

    s2w-demo skeleton

---
+++ data/js/app.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let pledgedSitesApp = angular.module("pledgedSitesApp", []);
+
+pledgedSitesApp.filter('escape', function() {
+  return window.escape;
+});
+
+pledgedSitesApp.controller("pledgedSitesCtrl", function($scope) {
+  $scope.pledgedSites = null;
+
+  self.port.on("pledgedSites", function(pledgedSites) {
+    $scope.$apply(_ => {
+      $scope.pledgedSites = JSON.stringify(pledgedSites, null, "  ");
+    });
+  });
+
+});
+
+pledgedSitesApp.controller("pledgePanelCtr", function($scope) {
+  $scope.pledge = function pledge() {
+    self.port.emit("pledged", $scope.siteInfo);
+  }
+
+  self.port.on("siteInfo", function(siteData) {
+    $scope.$apply(_ => {
+      $scope.siteInfo = siteData;
+    });
+  });
+});
+
+angular.bootstrap(document, ['pledgedSitesApp']);
+
+// Low-level data injection
+self.port.on("style", function(file) {
+  let link = document.createElement("link");
+  link.setAttribute("href", file);
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("type", "text/css");
+  document.head.appendChild(link);
+});
